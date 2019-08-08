4e4eeedabf1b7324d10e95a6b55cdaff01a5e7d0
atuldubey1 <51990075+atuldubey1@users.noreply.github.com>
2019-08-08 15:01:55 +0530

    doctorcontroller

--- OnlineClinic/src/main/java/com/mphasis/training/controller/DoctorController.java
+++ OnlineClinic/src/main/java/com/mphasis/training/controller/DoctorController.java
@@ -1,6 +1,31 @@
 package com.mphasis.training.controller;
 
+@RestController
+@RequestMapping("/doctor")
 public class DoctorController {
 	public void update() {
 }
+	@Autowired
+	DoctorBo doctorBo;
+	@RequestMapping(value = "/doctors", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Doctor> getAll() {
+		return doctorBo.getDoctors();
+
+	}
+	
+	@RequestMapping(value = "/doctors/{clinic}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Doctor> getDocbyClinic(@RequestBody Clinic clinic) {
+		return doctorBo.getDoctorByClinic(clinic);
+	}
+	@RequestMapping(value = "/delete/{dId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void deleteDoctor(@PathVariable("dId") int did) {
+		doctorBo.deleteDoctor(did);
+	}
+	@RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void insertDoctor(@RequestBody Doctor doctor) {
+		doctorBo.insertDoctor(doctor);
+	}
+	
+	
+	
 }
